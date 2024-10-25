9fb89736cd5c431719575b968e8fde61f8269f1a
cristhian <cristhiantombe@gmail.com>
2024-10-25 18:37:06 -0500

    title home page

--- app/home.tsx
+++ app/home.tsx
@@ -1,12 +1,57 @@
-import { View, Text, SafeAreaView } from 'react-native'
-import { Link } from 'expo-router'
 import React from 'react'
+import { View, Text, StyleSheet, Image  } from 'react-native'
+import { StatusBar } from 'expo-status-bar'
+import { SafeAreaView } from 'react-native-safe-area-context'
+import { heightPercentageToDP } from 'react-native-responsive-screen'
+
+import { Colors } from '../constants/Colors'
 
 export default function home() {
   return (
-    <SafeAreaView style={{flex: 1, justifyContent: 'center', alignItems: "center"}}>
-      <Text>home</Text>
-      <Link href={'/'}>Go root</Link>
+    <SafeAreaView style={style.container} edges={['top']}>
+      <StatusBar style="dark"></StatusBar>
+      <View style={style.containerText}>
+        <View  style={{gap: 5}}>
+          <Text style={style.firstText}>READY TO</Text>
+          <Text style={style.secondText}>WORKOUT</Text>
+        </View>
+        <View style={style.containerImage}>
+          <Image style={style.imgAvatar} source={require("../assets/images/avatar.png")}></Image>
+        </View>
+      </View>
+      
     </SafeAreaView>
   )
 }
+
+const style = StyleSheet.create({
+  container: {
+    flex: 1, 
+    marginTop: 20
+  },
+  containerText: {
+    flex: 1,
+    flexDirection: "row",
+    justifyContent: "space-between",
+    marginHorizontal: 15
+  },
+  firstText: {
+    fontSize: heightPercentageToDP(4.5),
+    fontWeight: "bold",
+    letterSpacing: 0.25,
+    color: Colors.textblack
+  },
+  secondText: {
+    fontSize: heightPercentageToDP(4.5),
+    fontWeight: "bold",
+    color: Colors.textred
+  },
+  containerImage: {
+    marginVertical: 10
+  },
+  imgAvatar: {
+    height: heightPercentageToDP(6),
+    width: heightPercentageToDP(6),
+    borderRadius: 100
+  }  
+})
---
+++ constants/Colors.ts
@@ -0,0 +1,5 @@
+
+export const Colors = {
+    textred: 'rgb(244 63 94)',
+    textblack: "#454545" 
+};
